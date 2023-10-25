01b1a7f6669e662e48facd267a6e93181b4f38d3
Kelvin Wu <kelvinyu1117@gmail.com>
2023-10-25 22:40:59 +0800

    add project 2 skeleton

---
+++ project2/include/lib/strat/common/strategy_traits.hpp
@@ -0,0 +1,8 @@
+#include <concepts>
+
+namespace strat::common {
+template <typename T>
+concept strategy_traits = requires(T strat) {
+
+};
+}
---
+++ project2/include/lib/strat/strategy_engine.hpp
@@ -0,0 +1,8 @@
+#include "lib/strategies/common/strategy_traits.hpp"
+
+namespace strat {
+template <common::strategy_traits TStrategy> class StrategyEngine {
+
+private:
+};
+} // namespace strat
